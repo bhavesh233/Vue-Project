be1a3e1f075b7d537eabfbedc8d5eb28e466f8ca
bhavesh233 <bhaveshk@prominentpixel.com>
2022-04-13 15:59:14 +0530

    lastest commit

---
+++ src/firebase/firebaseInit.js
@@ -0,0 +1,19 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAr5rLiDCXqsald8LmwxySAxuG9Q8HZFWE",
+  authDomain: "fireblog-fa8e4.firebaseapp.com",
+  projectId: "fireblog-fa8e4",
+  storageBucket: "fireblog-fa8e4.appspot.com",
+  messagingSenderId: "787144181490",
+  appId: "1:787144181490:web:d251a2c4cba0264644f26b",
+  measurementId: "G-BN5ZWRYP3W",
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const timestamp = firebase.firestore.FieldValue.serverTimestamp;
+
+export { timestamp };
+
+export default firebaseApp.firestore;
---
+++ src/views/Home.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="home">
+    <blog-post :post="welcomeScreen" />
+    <Blog-post :post="post" v-for="(post,index) in sampleBlogPost" :key="index"/>
+  </div>
+</template>
+
+<script>
+import BlogPost from "../components/BlogPost.vue";
+export default {
+  name: "Home",
+  components: { BlogPost },
+
+  data() {
+    return {
+      welcomeScreen: {
+        title: "welcome!",
+        blogPost:
+          "weekly blog articles with all things programming including HTML,CSS,JavaScript and more. Registor today to never miss a post",
+        welcomeScreen: true,
+        photo: "coding",
+      },
+
+      sampleBlogPost: [
+        {
+          title: "This is a Fillter Title",
+          blogHTML: "This is a filler blog post title",
+          blogCoverPhoto: "beautiful-stories",
+        },
+        {
+          title: "This is a Fillter Title",
+          blogHTML: "This is a filler blog post title!",
+          blogCoverPhoto: "designed-for-everyone",
+        },
+      ],
+    };
+  },
+};
+</script>
